e01a0c14dda9bb2caff9cb21eb97fb4a5ea4a24d
Carson Blaker <carson@Carsons-MacBook-Air.local>
2026-04-24 12:34:28 -0500

    deleted debug route

--- backend/server.js
+++ backend/server.js
@@ -36,12 +36,7 @@ app.get("/api", (req, res) => {
 app.use("/api/auth", authRoutes);
 app.use("/api/workouts", workoutRoutes);
 
-app.get("/api/debug", (req, res) => {
-  res.json({
-    jwtExists: !!process.env.JWT_SECRET,
-    mongoExists: !!process.env.MONGO_URI
-  });
-});
+
 
 const PORT = process.env.PORT || 3000;
 
